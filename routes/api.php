e6b9ff2687ad2b8b4dbdd2ce0e06e853dba374d0
أبو عبادة <aldyfwkymhmd@gmail.com>
2025-02-14 06:56:05 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\Api\ProductApiController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderApiController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -23,8 +24,8 @@ Route::middleware('auth:sanctum')->prefix('categories')->group(function () {
     Route::get('/{id}', [CategoryController::class, 'show']);
     Route::put('/{id}', [CategoryController::class, 'update']);
     Route::delete('/{id}', [CategoryController::class, 'destroy']);
-});
 
-Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('products', ProductApiController::class);
-});
+
+    Route::apiResource('orders', OrderApiController::class);
+}); 
